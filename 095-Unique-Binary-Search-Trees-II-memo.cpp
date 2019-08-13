75ff31cd6215894206da7fa681d307b9de210661
listenviolet <listenviolet@gmail.com>
2019-08-13 21:15:27 +0800

    going down and !

--- 095-Unique-Binary-Search-Trees-II-memo.cpp
+++ 095-Unique-Binary-Search-Trees-II-memo.cpp
@@ -46,3 +46,25 @@ public:
 // memo[i][j] 表示在区间 [i, j] 范围内可以生成的所有 BST 的根结点，
 // 所以 memo 必须是一个三维数组，这样在递归函数中，我们就可以去 memo 中查找当前的区间是否已经计算过了，
 // 是的话，直接返回 memo 中的数组，否则就按之前的方法去计算，最后计算好了之后要更新 memo 数组，
+
+// Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+// Example:
+
+// Input: 3
+// Output:
+// [
+//   [1,null,3,2],
+//   [3,2,null,1],
+//   [3,1,null,null,2],
+//   [2,1,3],
+//   [1,null,2,null,3]
+// ]
+// Explanation:
+// The above output corresponds to the 5 unique BST's shown below:
+
+//    1         3     3      2      1
+//     \       /     /      / \      \
+//      3     2     1      1   3      2
+//     /     /       \                 \
+//    2     1         2                 3
